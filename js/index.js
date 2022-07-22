281190bed536593f451bc9ab3240cc255a0266f4
Morshaedul <morshaedulanirbanit@gmail.com>
2022-07-22 08:22:49 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let x, y, z;  // Statement 1
+x = 10;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+
+document.getElementById("demo").innerHTML =
+"The value of z is " + z + ".";  
